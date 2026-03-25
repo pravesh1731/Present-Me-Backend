5bce3c7093a1ab4589e1c5b79327115181d209b5
pravesh1731 <706890pk@gmail.com>
2026-03-26 00:27:02 +0530

    port sent 5000

--- src/app.js
+++ src/app.js
@@ -53,5 +53,5 @@ app.use("/", notice);
 app.get("/", (req, res) => res.send("Present-Me back running"));
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
